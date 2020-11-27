e7c658c452070ebf0d5f8c7fa5346271144ad51a
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-11-27 12:00:06 -0800

    My_Common_Nostalgia_Triggers.py
    A file listing my common Nostalgia triggers.

---
+++ My_Common_Nostalgia_Triggers.py
@@ -0,0 +1,12 @@
+# Start of script
+nostalgiaTriggerList = ["Darkness", "Snow and ice", "Outer Space", "Pajamas", "Calmness", "Clouds", "Big cities", "Music", "Old games", "Other/unlisted"]
+print ("Seanpm2001's common nostalgia triggers:\n " + str(nostalgiaTriggerList))
+exit1 = input("Press [ENTER] key to close")
+print ("The window should now be closed. If the window isn't closed, use the close button. If this doesn't work, end the process/task with a process manager/task manager")
+# File info
+"""
+File version: 1 (Friday, November 27th 2020 at 12:00 pm)
+File type: Python script file (*.py)
+Line count (including blank lines and compiler line): 13
+"""
+# End of script
